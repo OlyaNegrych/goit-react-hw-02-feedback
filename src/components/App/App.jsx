05c3155b6e33050a31f5def8e5e7b103984be124
OlyaNegrych <olyanegrych@gmail.com>
2022-09-17 22:44:14 +0300

    started 1-st task

---
+++ src/components/App/App.jsx
@@ -0,0 +1,8 @@
+import Feedback from '../Feedback/feedback';
+// import { Statistics } from '../Statistics/Statistics';
+
+export const App = () => {
+  return (
+    <Feedback />
+  );
+};
---
+++ src/components/Feedback/Feedback.styled.jsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+// export const FriendListUl = styled.ul`
+//   list-style: none;
+//   padding: 10px;
+// `;
