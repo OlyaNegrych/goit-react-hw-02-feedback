3da3e43c97a7fe38189a2c0e5f8165055b985448
OlyaNegrych <olyanegrych@gmail.com>
2022-09-18 23:21:45 +0300

    shadow fix

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -7,6 +7,6 @@ export const Box = styled.div`
   font-size: 18px;
   font-weight: 500;
   border: #2a2a2a;
-  box-shadow: 10px 10px 22px 1px rgba(0, 0, 0, 0.55);
+  box-shadow: 1px 1px 25px 1px rgba(0, 0, 0, 0.55);
 
 `;
--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -7,7 +7,7 @@ export const FeedbackOptionBtn = styled.button`
   font-size: 18px;
   font-weight: 500;
   border: #2a2a2a;
-  box-shadow: 10px 10px 22px 1px rgba(0,0,0,0.55);
+  box-shadow: 1px 1px 25px 1px rgba(0,0,0,0.55);
 
   :active {
     background-color: #2e5ac5;
